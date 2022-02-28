85a3393d8387beb8f7378a27a48f2210e87ed07c
naptracks <cesar.martel9@gmail.com>
2022-02-28 12:09:12 +0100

    auth with github ok

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,31 +7,28 @@ import CredentialsProvider from "next-auth/providers/credentials"
 
 export default NextAuth({
     providers: [
-        // CredentialsProvider({
-        //     // The name to display on the sign in form (e.g. 'Sign in with...')
-        //     name: 'Credentials',
-        //
-        //     credentials: {
-        //         username: { label: "Username", type: "text", placeholder: "jsmith" },
-        //         password: {  label: "Password", type: "password" }
-        //     },
-        //     async authorize(credentials, req) {
-        //
-        //         const res = await fetch("http://localhost:3000/api/fakeAuth", {
-        //             method: 'POST',
-        //             body: JSON.stringify(credentials),
-        //             headers: { "Content-Type": "application/json" }
-        //         })
-        //         const user = await res.json()
-        //
-        //         // If no error and we have user data, return it
-        //         if (res.ok && user) {
-        //             return user
-        //         }
-        //         // Return null if user data could not be retrieved
-        //         return null
-        //     }
-        // }),
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. 'Sign in with...')
+            name: 'Credentials',
+
+
+            async authorize(credentials, req) {
+
+                const res = await fetch("http://localhost:3000/api/fakeAuth", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                })
+                const user = await res.json()
+
+                // If no error and we have user data, return it
+                if (res.ok && user) {
+                    return user
+                }
+                // Return null if user data could not be retrieved
+                return null
+            }
+        }),
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
